31aa68baa4955af4575312dd3eefb9efae97fc8d
Pascal Mathys <pascal.mathys@rootix.ch>
2022-12-01 18:28:38 +0100

    day 1

---
+++ days/day1.ts
@@ -0,0 +1,21 @@
+﻿import { d1i } from '../data'
+import { splitLinesByEmptyLines, splitLinesIntoArray, sum, sortDesc } from '../utils'
+
+export function q1() {
+    const aggregatedValues = splitLinesByEmptyLines(d1i)
+        .map(splitLinesIntoArray)
+        .map(group => group.map(Number))
+        .map(group => sum(group));
+    console.log('d1q1: ' + Math.max(...aggregatedValues));
+}
+
+export function q2() {
+    const aggregatedValues = sortDesc(splitLinesByEmptyLines(d1i)
+        .map(splitLinesIntoArray)
+        .map(group => group.map(Number))
+        .map(group => sum(group)));
+    console.log('d1q2: ' + sum(aggregatedValues.slice(0, 3)));
+}
+
+q1();
+q2();
---
+++ utils.ts
@@ -0,0 +1,14 @@
+﻿export function splitLinesByEmptyLines(input: string) {
+    return input.split('\n\n');
+}
+
+export function splitLinesIntoArray(input: string) {
+    return input.split('\n');
+}
+export function sum(array: number[]) {
+    return array.reduce((total, current) => total + current);
+}
+
+export function sortDesc(array: number[]) {
+    return array.sort((a, b) => a > b ? -1 : a < b ? 1 : 0);
+}
